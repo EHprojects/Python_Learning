6b4cc76c38fa2c2c1cd7bb1d76987113ff00a3dc
Eric H <elharvey234@gmail.com>
2021-11-08 15:46:49 -0800

    CoffeeMachineOOP Update

--- CoffeeMachineOOP/main.py
+++ CoffeeMachineOOP/main.py
@@ -4,31 +4,31 @@ from money_machine import MoneyMachine
 
 
 def coffee_machine():
-    drinks = Menu()
-    machine = CoffeeMaker()
-    money = MoneyMachine()
+    menu = Menu()
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
 
     machine_on = True
 
     while machine_on:
         # Prompt User for Selection
-        selection = input("What would you like? " + "(" + drinks.get_items() + "): ").lower()
+        selection = input(f"What would you like? {menu.get_items()}: ").lower()
 
         # Perform Actions
         if selection == "off":
             machine_on = False
         elif selection == "report":
-            machine.report()
-            money.report()
+            coffee_maker.report()
+            money_machine.report()
         elif selection == "espresso" or selection == "latte" or selection == "cappuccino":
             # Create the drink order
-            new_order = drinks.find_drink(selection)
+            new_order = menu.find_drink(selection)
             # Check resources
-            if machine.is_resource_sufficient(new_order):  # If there are sufficient resources, proceed with the order
+            if coffee_maker.is_resource_sufficient(new_order):  # If resources are sufficient, proceed with order
                 # Process the coins
-                if money.make_payment(new_order.cost):  # If payment is successful, proceed with the order
+                if money_machine.make_payment(new_order.cost):  # If payment is successful, proceed with order
                     # Make the order
-                    machine.make_coffee(new_order)
+                    coffee_maker.make_coffee(new_order)
         else:
             print("Invalid selection")
 
