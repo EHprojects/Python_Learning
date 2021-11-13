649995b5b497299fdaaa6ec7d6c2ffdce696b87b
Eric H <elharvey234@gmail.com>
2021-11-13 12:39:59 -0800

    PongGame update

--- PongGame/main.py
+++ PongGame/main.py
@@ -1,31 +1,23 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Ping? Pong!")
+screen.tracer(0)
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-# Right Paddle: wid = 20, hei = 100, xpos = 350, ypos = 0
-# up / dn to control paddle 20 px mvmnt
-
-def forward():
-    r_paddle.forward(20)
-
-
-def backward():
-    r_paddle.backward(20)
-
-
-r_paddle = Turtle()
-r_paddle.color("white")
-r_paddle.shape("square")
-r_paddle.penup()
-r_paddle.setheading(90)
-r_paddle.shapesize(stretch_len=5)
-r_paddle.setpos(x=350, y=0)
 screen.listen()
-screen.onkey(r_paddle.forward, "Up")
-screen.onkey(r_paddle.backward, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -34,7 +34,7 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.head.forward(MOVE_DISTANCE)
+        self.head.go_up(MOVE_DISTANCE)
 
     def up(self):
         if self.head.heading() != DOWN:
