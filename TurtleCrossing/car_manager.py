21d3e01851f3df93f4f9b8536b61b303e410f96f
Eric H <elharvey234@gmail.com>
2021-11-14 19:28:22 -0800

    TurtleCrossing Update

--- TurtleCrossing/car_manager.py
+++ TurtleCrossing/car_manager.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color(random.choice(COLORS))
+        self.shapesize(stretch_len=2)  # stretch length to 40px
+        self.penup()
+
+        # Car buffer zone = 50 px from top & bottom of screen (-240 to 240 for 20px cars)
+        # start cars just off screen (x = 320)
+        self.goto(x=320, y=random.randint(-240, 240))
+
+    def move(self):
+        self.setx(self.xcor() - STARTING_MOVE_DISTANCE)
--- TurtleCrossing/main.py
+++ TurtleCrossing/main.py
@@ -6,9 +6,31 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("white")
+screen.title("Definitely Not Frogger")
 screen.tracer(0)
+
+player = Player()
+cars = []
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
+loop_pass = 0
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    loop_pass += 1
+
+    if loop_pass % 6 == 0:
+        cars.append(CarManager())
+        loop_pass = 0
+
+    for car_obj in cars:
+        car_obj.move()
+
     screen.update()
+
+
+
