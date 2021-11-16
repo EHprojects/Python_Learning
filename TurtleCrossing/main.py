326a1e1315825d6e8720a655b6a583bcfb0897f7
Eric H <elharvey234@gmail.com>
2021-11-15 17:33:21 -0800

    TurtleCrossing Update

--- TurtleCrossing/main.py
+++ TurtleCrossing/main.py
@@ -32,7 +32,7 @@ while game_is_on:
         loop_pass = 0
 
     for car in car_manager.all_cars:
-        if player.distance(car) < 30:
+        if player.distance(car) < 20:
             game_is_on = False
             scoreboard.game_over()
 
