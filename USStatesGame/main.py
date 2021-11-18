b7a803fd198695bafc68c6295376a1fe5cfceac4
Eric H <elharvey234@gmail.com>
2021-11-17 21:16:35 -0800

    U.S. States Game Update

--- USStatesGame/main.py
+++ USStatesGame/main.py
@@ -1,5 +1,7 @@
 import turtle
-import pandas, name_writer
+import csv
+import name_writer
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -15,12 +17,22 @@ state_names = states_data.state.to_list()
 correct_answers = []
 
 while len(correct_answers) < 50:
-    answer_state = screen.textinput(title=f"{len(correct_answers)}/50 States Correct", prompt="What's another state's "
-                                                                                              "name?")
+    answer_state = screen.textinput(title=f"{len(correct_answers)}/50 States Correct",
+                                    prompt="What's another state's name?")
     answer_state = answer_state.title()
+    if answer_state == "Exit":
+        break
     if answer_state in state_names and answer_state not in correct_answers:
         correct_answers.append(answer_state)
         x_coord = int(states_data[states_data["state"] == answer_state].x)
         y_coord = int(states_data[states_data["state"] == answer_state].y)
         name_writer.write_name(name=answer_state, x=x_coord, y=y_coord)
 
+states_to_learn = []
+
+for state in state_names:
+    if state not in correct_answers:
+        states_to_learn.append(state)
+
+data = pandas.Series(states_to_learn)
+data.to_csv("states_to_learn.csv")
