6856457095847725110fad02da6da2e651306fc8
Eric H <elharvey234@gmail.com>
2021-11-18 18:48:04 -0800

    U.S. States Game updated with list comprehension

--- USStatesGame/main.py
+++ USStatesGame/main.py
@@ -28,11 +28,11 @@ while len(correct_answers) < 50:
         y_coord = int(states_data[states_data["state"] == answer_state].y)
         name_writer.write_name(name=answer_state, x=x_coord, y=y_coord)
 
-states_to_learn = []
+states_to_learn = [state for state in state_names if state not in correct_answers]
 
-for state in state_names:
-    if state not in correct_answers:
-        states_to_learn.append(state)
+# for state in state_names:
+#     if state not in correct_answers:
+#         states_to_learn.append(state)
 
 data = pandas.Series(states_to_learn)
 data.to_csv("states_to_learn.csv")
