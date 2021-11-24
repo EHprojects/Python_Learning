c607150fb3cb1ca720e2c7eca1c7dbee21222255
Eric H <elharvey234@gmail.com>
2021-11-23 18:39:41 -0800

    NATA Alpha Update

--- NATOAlphabet/main.py
+++ NATOAlphabet/main.py
@@ -5,17 +5,17 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
 
-valid_input = False
 
-while not valid_input:
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
     try:
-        word = input("Enter a word: ").upper()
         output_list = [phonetic_dict[letter] for letter in word]
     except KeyError:
         print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
     else:
-        valid_input = True
         print(output_list)
 
+
+generate_phonetic()
